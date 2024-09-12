9ff51a26241f548180822789d2ee87c5099e199d
Alesssandra F R <arfurlanetto@gmail.com>
2024-09-11 22:41:16 -0300

    2

---
+++ cad.js
@@ -0,0 +1,20 @@
+/*let - variável com escolpo em bloco
+get= método de solicitar "pegar a informação", procurar, obter informação
+post = enviar informação, que que será posta no bando de dado ! pode atualizar, mas não é exatamente correto
+put = atualizar, altera, dado já existe e será modificado
+delete = apaga registro
+por isso colocamos get
+{} tudo quee stá dentro é objeto
+fetch(url,{metodo:post (ou get ou put), body:json.stringify (converte o json({coloca infomações do cod}), headers:{envia json e recebe json })
+await aguarda a requisição - para ser async - função assíncrona, "não contante" 
+depois faz conferimento com if, neste caso*/
+
+function cadastrar(){
+
+          let name = document getElementByld("name").value
+          let cpf_cnpj =
+          let email
+          let password =
+
+
+}
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-/* parâmetros , key + value - ex nome + calos, no enderço aparecerá ?keyvalor, depois adiciona & - paf  /key*/
+/* header informaçõe do "cabeçalho" que usa key valor e ? &
+ex: https://www.google.com/  search ? q=como+por+campo+obrigat%C3%B3rio+html & sca_esv=708ca891a389fdf3...
+? key valor, se tiver outro parâmetro &, se mais outra continua com &
+isso se chama query params
+pas param - / valo / valo - coloca como valor exto da url
+authorization - autenticar e obtem toquem que pode ser enviado
+body corpo - onde ficam informações
+*/
